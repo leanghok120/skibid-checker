8bce62a9263fb4c1dd3059574069c395d667cff6
leanghok120 <leanghokoeng5@gmail.com>
2024-12-05 04:48:45 +0700

    Add a save image button

--- app/routes/results.$username.tsx
+++ app/routes/results.$username.tsx
@@ -1,5 +1,7 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { toPng } from "html-to-image";
+import { useRef } from "react";
 import { analyzeUser } from "~/utils/analyzeUser";
 import { getUserData } from "~/utils/getUserData";
 
@@ -12,10 +14,24 @@ export async function loader({ params }) {
 
 export default function Results() {
   const { userData, analysis } = useLoaderData<typeof loader>();
+  const cardRef = useRef(null);
+
+  async function handleSaveImage() {
+    if (cardRef.current) {
+      const dataUrl = await toPng(cardRef.current);
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = `${userData.username}`;
+      link.click();
+    }
+  }
 
   return (
     <div className="flex flex-col h-screen items-center justify-center">
-      <div className="border-gray-900 border-2 p-5 rounded-xl w-full max-w-96">
+      <div
+        className="border-gray-900 border-2 p-5 rounded-xl w-full max-w-96 mb-8"
+        ref={cardRef}
+      >
         <div className="flex items-center gap-4 mb-4">
           <img
             src={userData.avatar}
@@ -30,6 +46,12 @@ export default function Results() {
         <p className="text-gray-500 mb-5">{userData.biography}</p>
         <p>{analysis?.analysis}</p>
       </div>
+      <button
+        className="border-2 border-gray-900 p-3 rounded-xl font-bold"
+        onClick={handleSaveImage}
+      >
+        Save image
+      </button>
     </div>
   );
 }
